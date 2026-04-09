2e4efad92d58f4b0192b4a342aac293a3330165c
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-09 19:48:52 +0600

    Git Commit 9-4-26

--- script/login.js
+++ script/login.js
@@ -1,5 +1,9 @@
 console.log("jeyaul ul hoque"); 
 
 document.getElementById("login-btn").addEventListener("click", function(){
-    console.log("login button clicked")
-})
+    // 1- get the monile number
+    // 2 - get the pin input
+    // 3- matvh pin & monile number
+    // 3-1 true:::>> alert > homepage
+    // 3-2 true:::>> alert> returr
+});
