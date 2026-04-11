0b5f99bcf95cca960abae3aab2773d70d32cd1d0
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-12 00:39:37 +0600

    Git Commit 12-4-26

--- script/addMoney.js
+++ script/addMoney.js
@@ -2,4 +2,8 @@ document.getElementById("add-money-btn").addEventListener("click",function(){
     //1- bank account ger
     const bankAccount = getValueFromeInput("add-money-bank");
     console.log(bankAccount)
+    if(bankAccount == "Select A Bank"){
+        alert("Please select a bank")
+        return;
+    }
 })
