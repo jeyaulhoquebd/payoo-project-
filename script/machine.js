72981b62eaa68d171fb62bb91201e8a29092952c
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-11 22:31:06 +0600

    Git Commit 11-4-26

---
+++ script/machine.js
@@ -0,0 +1,8 @@
+console.log("jeya")
+
+// machine id -> input value
+function getValueFromeInput(id){
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(id , value);
+}
