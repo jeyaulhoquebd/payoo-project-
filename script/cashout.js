793345e3184dd82c454cd78a4512257b0b9bf494
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-11 21:22:36 +0600

    Git Commit 11-4-26

--- script/cashout.js
+++ script/cashout.js
@@ -12,7 +12,10 @@ document.getElementById("cashout-btn").addEventListener("click", function(){
     const balanceElement = document.getElementById("balance")
     const balance = balanceElement.innerText;
     console.log(balance)
+
     // 4- Calculate new Balance 
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    console.log("New Balance 👉" , newBalance);
     // 5- Get the Pin and verify 
     // 5-1 true :: show an alert > set Balance
     // 5-2 true :: show anerror alert > return
