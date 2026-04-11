770ae3243b874e65a08fc95b0234a92403a30ddb
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-11 20:05:56 +0600

    Git Commit 11-4-26

--- script/cashout.js
+++ script/cashout.js
@@ -1,1 +1,9 @@
-document.getElementById("cashout-btn");
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    //1- get the agent number & validate
+    // 2- get the amount , valideat , convetyto Number
+    // 3- get the Current Balance , valideat , convetyto Number
+    // 4- Calculate new Balance 
+    // 5- Get the Pin and verify 
+    // 5-1 true :: show an alert > set Balance
+    // 5-2 true :: show anerror alert > return
+});
