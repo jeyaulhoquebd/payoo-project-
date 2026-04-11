1d48d7eecba301e2f72c9f446dc7129be94e7c63
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-11 21:16:09 +0600

    Git Commit 11-4-26

--- script/cashout.js
+++ script/cashout.js
@@ -3,7 +3,11 @@ document.getElementById("cashout-btn").addEventListener("click", function(){
    const cashoutNumberInput = document.getElementById("cashout-number");
    const cashoutNumber = cashoutNumberInput.value;
    console.log(cashoutNumber);
-    // 2- get the amount , valideat , convetyto Number
+
+    // 2- get the amount 
+    const cashoutAmountInput = document.getElementById("cashout-amount")
+    const cashoutAmount = cashoutAmountInput.value;
+    console.log(cashoutAmount);
     // 3- get the Current Balance , valideat , convetyto Number
     // 4- Calculate new Balance 
     // 5- Get the Pin and verify 
