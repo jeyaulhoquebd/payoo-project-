a0ff64537a3c19fb0bfcf23d30cdee1500a38251
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-12 20:16:48 +0600

    Git Commit 12-4-26

--- script/addMoney.js
+++ script/addMoney.js
@@ -16,7 +16,7 @@ document.getElementById("add-money-btn").addEventListener("click",function(){
     //3- get ammount 
     const ammount = getValueFromeInput("add-money-amount");
     const currentBalance = getBalance()
-    const newBlance =  + Number(ammount);
+    const newBlance = currentBalance + Number(ammount);
     
     const pin = getValueFromeInput("add-money-pin");
     if(pin == "1234"){
