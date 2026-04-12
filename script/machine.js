20da5e9efaa4c3030cf01759692c8869d018a12c
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-04-12 21:41:14 +0600

    Git Commit 12-4-26

--- script/machine.js
+++ script/machine.js
@@ -23,3 +23,10 @@ function setBalance(value){
     const balanceElement = document.getElementById("balance");
     balanceElement.innerText = value;
 }
+
+// machine id > hide all > show id
+function showOnly(id){
+    const addmoney = document.getElementById("add-money")
+    const cashout = document.getElementById("Cashout")
+
+}
